77f00fb94bfc2ca65fc7ff1d0a378e2ebf8774ae
lucamelao <63018319+lucamelao@users.noreply.github.com>
2022-10-20 20:06:00 -0300

    A+

--- Projeto_01/Projeto1.vhd
+++ Projeto_01/Projeto1.vhd
@@ -250,8 +250,9 @@ BLOCK_OUT <= decoder_block_out;
 ADDRESSES_OUT <= decoder_address_out;
 ROM_ADDR <= rom_address;
 
--- LEDR(ADDRESS_SIZE-1 downto 0) <= rom_address;
-LEDR(9) <= clr_1;
+LEDR(DATA_SIZE-1 downto 0) <= led_R;
+LEDR(8) <= led_8;
+LEDR(9) <= led_9;
 ---------------------------------------------------------------
 --LEDR(8) <= led_8;
 --LEDR(9) <= led_9;

87d75bf42d4f40e5a59094fadf78375f32085757
lucamelao <63018319+lucamelao@users.noreply.github.com>
2022-11-03 13:43:00 -0300

    1 sec working

--- Projeto_01_1/python/main.py
+++ Projeto_01_1/python/main.py
@@ -2,7 +2,7 @@ from Assembler import Assembler
 
 if __name__ == '__main__':
     dir = "code/"
-    file = ""
+    file = "_1SEC"
     in_file = f"ASM/ASM{file}.txt"
     out_file = f"BIN/BIN{file}.txt"
     a =  Assembler(9, 3 ,dir + in_file, dir + out_file, '!')
